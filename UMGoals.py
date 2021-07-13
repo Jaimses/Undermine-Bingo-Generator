12b670d2bba1075dfba9ff9dec15eb7013de747f
Jaimses <77019197+Jaimses@users.noreply.github.com>
2021-07-13 10:45:19 +0100

    Fixes
    JSON output still requires some edits, but is much easier to edit into the required format. Annoying that the website requires the formatting the way it does but there isn't much more I can do. Now outputs goals with a unique ID attached.

--- UMGoals.py
+++ UMGoals.py
@@ -4,7 +4,7 @@ import json
 E = 16
 M = 6
 H = 3
-bingodict = {"name":[]}
+bingodict = {}
 with open('UMGoals.csv') as csv_file:
     csv_reader = csv.DictReader(csv_file, delimiter=',')
     csv_lines = sum(1 for line in csv_reader)
@@ -27,31 +27,31 @@ with open('UMGoals.csv') as csv_file:
                print("oh no")
           else:
                if line_count in EGoals and line_count in MGoals and line_count in HGoals:
-                    bingodict["name"].append(row[0])
-                    bingodict["name"].append(row[1])
-                    bingodict["name"].append(row[2])
-                    print("123")
+                    bingodict["name"+str(line_count)+"EMH"] = (row[0])
+                    bingodict["name"+str(line_count)+"MEH"] = (row[1])
+                    bingodict["name"+str(line_count)+"HEM"] = (row[2])
+                    print("EMH")
                elif line_count in EGoals and line_count in MGoals:
-                    bingodict["name"].append(row[1])
-                    bingodict["name"].append(row[0])
-                    print("12")
+                    bingodict["name"+str(line_count)+"ME"] = (row[1])
+                    bingodict["name"+str(line_count)+"EM"] = (row[0])
+                    print("EM")
                elif line_count in HGoals and line_count in EGoals:
-                    bingodict["name"].append(row[2])
-                    bingodict["name"].append(row[0])
-                    print("13")
+                    bingodict["name"+str(line_count)+"HE"] = (row[2])
+                    bingodict["name"+str(line_count)+"EH"] = (row[0])
+                    print("EH")
                elif line_count in HGoals and line_count in MGoals:
-                    bingodict["name"].append(row[1])
-                    bingodict["name"].append(row[2])
-                    print("23")
+                    bingodict["name"+str(line_count)+"MH"] = (row[1])
+                    bingodict["name"+str(line_count)+"HM"] = (row[2])
+                    print("MH")
                elif line_count in HGoals:
-                    bingodict["name"].append(row[2])
-                    print("3")
+                    bingodict["name"+str(line_count)+"H"] = (row[2])
+                    print("H")
                elif line_count in MGoals:
-                    bingodict["name"].append(row[1])
-                    print("2")
+                    bingodict["name"+str(line_count)+"M"] = (row[1])
+                    print("M")
                elif line_count in EGoals:
-                    bingodict["name"].append(row[1])
-                    print("1")
+                    bingodict["name"+str(line_count)+"E"] = (row[0])
+                    print("E")
                else:
                     print(line_count)
                line_count += 1
